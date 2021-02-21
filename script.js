39920b79639f8305ad416491ab06a0388d247af6
Christian Castaneda <christianjc_09@berkeley.edu>
2021-02-20 19:30:11 -0600

    update 2

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function changePic() {
     var pic = document.querySelector('img');
+    console.log(pic);
     if (pic.src == "/img/nature.jpeg") {
-        console.log(pic.src)
+        console.log(pic.src);
     } else {
-        console.log("src does not match")
+        console.log("src does not match");
     }
 }
